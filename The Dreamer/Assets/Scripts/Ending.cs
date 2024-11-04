5c739a1a53a8809780071c4844663bcdf4727df4
jdotson717 <allenj13@miamioh.edu>
2024-11-04 10:18:45 -0800

    Final Push!
    Fade between post-processing volumes, fade between songs at the end, other minor adjustments.

--- The Dreamer/Assets/Scripts/Ending.cs
+++ The Dreamer/Assets/Scripts/Ending.cs
@@ -7,6 +7,9 @@ using TMPro;
 public class Ending : MonoBehaviour
 {
 
+	[SerializeField] private AudioSource mainLoop = null;
+	[SerializeField] private AudioSource heavenMusic = null;
+
 	public Image dot;
     public void EndGame( ) {
 		Application.Quit();
@@ -15,10 +18,29 @@ public class Ending : MonoBehaviour
 	public void OnCollisionEnter(Collision other) {
 		Debug.Log( "Collide" );
 
+		StartCoroutine(FadeTrack());
+
 		if(other.gameObject.CompareTag("Player")) {
 			Debug.Log( "Collide Collide With Player" );
 			this.gameObject.GetComponent<Animator>().enabled = true;
 			dot.gameObject.SetActive( false );
 		}
+	}
+
+	private IEnumerator FadeTrack()
+    {
+		float timeToFade = 1f;
+		float timeElapsed = 0;
+
+		heavenMusic.Play();
+
+		while (timeElapsed < timeToFade)
+        {
+			heavenMusic.volume = Mathf.Lerp(0, 1, timeElapsed / timeToFade);
+			mainLoop.volume = Mathf.Lerp(1, 0, timeElapsed / timeToFade);
+			timeElapsed += Time.deltaTime;
+			yield return null;
+        }
+		mainLoop.Stop();
 	}
 }
